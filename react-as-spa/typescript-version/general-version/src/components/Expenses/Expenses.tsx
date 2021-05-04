29dbfbd18f5a8d0cf5b8f5fb296d6806ee0755e8
johannesstroebele91 <stroebele.j@gmail.com>
2021-05-04 16:25:48 +0200

    Outsourced code of App into Expenses component for better overview

---
+++ react-as-spa/typescript-version/general-version/src/components/Expenses/Expenses.tsx
@@ -0,0 +1,35 @@
+import ExpenseItem from "../ExpenseItem/ExpenseItem";
+import "./Expenses.css";
+import Expense from '../../models/types';
+
+interface ExpensesProps {
+  expenses: Expense[];
+}
+
+const Expenses: React.FC<ExpensesProps> = ({expenses}) => {
+  return (
+    <div className="expenses">
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseItem>
+    </div>
+  );
+};
+export default Expenses;
