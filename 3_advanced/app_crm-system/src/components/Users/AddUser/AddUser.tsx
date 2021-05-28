9af1f38e2ac66ae937bcd51ae0f5f068f3101671
johannesstroebele91 <stroebele.j@gmail.com>
2021-05-28 17:15:51 +0200

    Add explanation for useRef(), which is an alternative to useState for managing user input

--- 3_advanced/app_crm-system/src/components/Users/AddUser/AddUser.tsx
+++ 3_advanced/app_crm-system/src/components/Users/AddUser/AddUser.tsx
@@ -2,7 +2,8 @@
 import classes from "./AddUser.module.css";
 import Card from "../../UI/Card/Card";
 import Button from "../../UI/Button/Button";
-import { useState } from "react";
+// 1) Import useRef
+import { useState, useRef } from "react";
 import ErrorModal from "../../UI/Modal/ErrorModal";
 import Wrapper from "../../Helpers/Wrapper";
 
@@ -11,8 +12,13 @@ interface AdduserProps {
 }
 
 const AddUser: React.FC<AdduserProps> = ({ onAddUser }) => {
-  const [enteredUsername, setEnteredUsername] = useState("");
-  const [enteredAge, setEnteredAge] = useState("");
+  // !!! useRef in 6 steps
+
+  // 2) Declare useRef()
+  const usernameInputRef = useRef<HTMLInputElement>(null);
+  const ageInputRef = useRef<HTMLInputElement>(null);
+
+  // useState
   const [showErrorModal, setShowErrorModal] = useState(false);
   const [errorModal, setErrorModal] = useState<{
     title: string;
@@ -23,32 +29,47 @@ const AddUser: React.FC<AdduserProps> = ({ onAddUser }) => {
     // Call preventDefault() to prevent page reload (automatic HTTP request)
     event.preventDefault();
 
-    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
-      setErrorModal({
-        title: "Invalid input",
-        message: "No username was entered",
-      });
-      setShowErrorModal(true);
-      return;
+    // 4) Reading data from the input
+    const enteredUsername = usernameInputRef.current?.value;
+    const enteredAge = ageInputRef.current?.value;
+
+    // 5) Use value
+    if (enteredUsername !== undefined || enteredAge !== undefined) {
+      // Check for refs not being undefined
+      if (
+        enteredUsername!.trim().length === 0 ||
+        enteredAge!.trim().length === 0
+      ) {
+        setErrorModal({
+          title: "Invalid input",
+          message: "No username was entered",
+        });
+        setShowErrorModal(true);
+        return;
+      }
+      if (+enteredAge! < 1) {
+        setErrorModal({
+          title: "Invalid age",
+          message: "The age of the user is invalid",
+        });
+        setShowErrorModal(true);
+        return;
+      }
+      // Pass new user to the parent (cast age to number)
+      onAddUser(enteredUsername!, +enteredAge!);
+
+      // 6) WARNING: Reset the value directly in the DOM
+      // Such operations should normally be only done by React
+      // THEREFORE this is an edge case
+      usernameInputRef.current!.value = "";
+      ageInputRef.current!.value = "";
     }
-    if (+enteredAge < 1) {
-      setErrorModal({
-        title: "Invalid age",
-        message: "The age of the user is invalid",
-      });
-      setShowErrorModal(true);
-      return;
-    }
-    // Pass new user to the parent (cast age to number)
-    onAddUser(enteredUsername, +enteredAge);
-    setEnteredUsername("");
-    setEnteredAge("");
   };
 
   const closeModalHandler = () => {
     setShowErrorModal(false);
     if (errorModal.message === "The age of the user is invalid") {
-      setEnteredAge("");
+      ageInputRef.current!.value = "";
     }
   };
 
@@ -66,19 +87,10 @@ const AddUser: React.FC<AdduserProps> = ({ onAddUser }) => {
         <form onSubmit={addUserHandler}>
           {/* htmlFor connects label to the input id*/}
           <label htmlFor="username">Username</label>
-          <input
-            id="username"
-            type="text"
-            onChange={(event: any) => setEnteredUsername(event.target.value)}
-            value={enteredUsername}
-          ></input>
+          {/* 3) Use ref to connect the TS to the HTML element */}
+          <input id="username" type="text" ref={usernameInputRef}></input>
           <label htmlFor="age">Age (Years)</label>
-          <input
-            id="age"
-            type="number"
-            onChange={(event: any) => setEnteredAge(event.target.value)}
-            value={enteredAge}
-          ></input>
+          <input id="age" type="number" ref={ageInputRef}></input>
           <Button type="submit">Add User</Button>
         </form>
       </Card>
---
+++ 3_advanced/state/useRef.md
@@ -0,0 +1,112 @@
+- [1) Basics](#1-basics)
+- [2) Uncontrolled component](#2-uncontrolled-component)
+- [3) Approach](#3-approach)
+- [4) Use cases](#4-use-cases)
+- [5) Issues resetting useRef](#5-issues-resetting-useref)
+- [6) Example](#6-example)
+
+# 1) Basics
+
+Refs enable to getting user input (like useState)
+
+- so it gives you access to other DOM element
+- and enables you to work with them
+- (setup a connection btw a HTML element and JS/TS code)
+
+# 2) Uncontrolled component
+
+useRef enables to build uncontrolled component
+
+- where the internal state (value in them)
+- is not controlled by React
+- but it relies on the default behavior of the input
+- and the value is just fetched by a React feature
+
+# 3) Approach
+
+1. import { useRef } from "react";
+2. declare ref using useRef();
+   1. initialize with null and tell TypeScript that an HTMLInputElement is expected
+   2. (e.g. `const usernameInputRef = useRef<HTMLInputElement>(null);`)
+3. connect TS to HTML element using ref={} attribute (e.g. `<input ref={usernameInputRef}>`)
+4. Reading data from the input (e.g. `const enteredUsername = usernameInputRef.current?.value;`)
+5. Use the value `enteredUsername` as needed
+6. WARNING Reset the inputs directly by manipulating the DOM (e.g. `usernameInputRef.current!.value = "";`)
+
+# 4) Use cases
+
+Simlar as for useState for e.g. managing inputs
+
+- BUT it is much quicker than useState
+- if the goal is to only READ a value
+- NOT write it
+
+# 5) Issues resetting useRef
+
+To reset useRef
+
+- you need to either manipulate the DOM
+- e.g. `usernameInputRef.current!.value = "";`
+- which is bad practice
+  - you should leave such stuff normally to React,
+  - but here not possible
+
+Alternatively this can be done using
+
+- a reset function on the form (e.g. `formRef.current.reset();` )
+- which needs to be reflected in the HTML (e.g. `form ref={formRef}`)
+- Ref: https://stackoverflow.com/a/62413386/11823182
+
+# 6) Example
+
+useRef can be used using these 6 steps:
+
+```javascript
+  // 1) import useRef
+import { useRef } from "react";
+
+const AddUser: React.FC<AdduserProps> = ({ onAddUser }) => {
+
+  // 2) Declare useRef()
+  const usernameInputRef = useRef<HTMLInputElement>(null);
+  const ageInputRef = useRef<HTMLInputElement>(null);
+
+  const addUserHandler = (event: any) => {
+    event.preventDefault();
+
+    // 4) Reading data from the input
+    const enteredUsername = usernameInputRef.current?.value;
+    const enteredAge = ageInputRef.current?.value;
+
+   // 5) Use the value
+    if (enteredUsername !== undefined || enteredAge !== undefined) {
+      }
+      // Pass new user to the parent (cast age to number)
+      onAddUser(enteredUsername!, +enteredAge!);
+
+      // 6) WARNING: Reset the value directly in the DOM
+      // Such operations should normally be only done by React
+      // THEREFORE this is an edge case
+      usernameInputRef.current!.value = "";
+      ageInputRef.current!.value = "";
+    }
+  };
+
+  };
+
+  return (
+    <>
+      <Card className={classes.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">Username</label>
+          {/* 3) Use ref to connect the TS to the HTML element */}
+          <input id="username" type="text" ref={usernameInputRef}></input>
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
+    <>
+  );
+};
+
+
+```
