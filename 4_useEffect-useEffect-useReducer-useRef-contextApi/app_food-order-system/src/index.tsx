7a355805815a45b3737feaf81ced04c3c757927c
johannesstroebele91 <stroebele.j@gmail.com>
2021-06-12 19:26:59 +0200

    Add basic predefined files for food order app

---
+++ 4_useEffect-useEffect-useReducer-useRef-contextApi/app_food-order-system/src/index.tsx
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+  // references the div id="root" in the index.html of the public folder
+ReactDOM.render(<App/>, document.getElementById('root'));
