e1d27ef2d405864595f8474407becfa1752d028e
johannesstroebele91 <stroebele.j@gmail.com>
2021-09-01 10:17:12 +0200

    Add logic for managing overall form validity

--- 8_handling forms/app_form-handler/src/components/SimpleInput.js
+++ 8_handling forms/app_form-handler/src/components/SimpleInput.js
@@ -3,9 +3,14 @@ import { useState } from "react";
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState("");
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
-
   const enteredNameIsValid = enteredName.trim() !== "";
   const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
+  let formIsValid = false;
+
+  if (enteredNameIsValid) {
+    formIsValid = true;
+  }
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
@@ -49,7 +54,7 @@ const SimpleInput = (props) => {
         )}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
